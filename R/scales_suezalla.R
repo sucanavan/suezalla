783f6bf6e4044dc97f68b3c7f8d15141441f3cba
sucanavan <48557623+sucanavan@users.noreply.github.com>
2025-05-03 15:20:43 +0100

    Update scales_suezalla.R

--- R/scales_suezalla.R
+++ R/scales_suezalla.R
@@ -1,9 +1,13 @@
 #' Custom ggplot2 colour scale for suezalla palettes
 #'
-#' @param palette_name Name of the palette to use
-#' @param n Number of colours to use
-#' @param type "discrete" or "continuous"
-#' @param ... Additional arguments passed to scale functions
+#' Applies a named suezalla palette to the colour aesthetic in ggplot2.
+#'
+#' @param palette_name Name of the palette to use (e.g., "jam", "retro")
+#' @param n Number of colours to use. If NULL, uses full palette length.
+#' @param type Either "discrete" or "continuous"
+#' @param ... Additional arguments passed to scale_colour_manual() or scale_colour_gradientn()
+#'
+#' @return A ggplot2 scale object
 #' @export
 scale_colour_suezalla <- function(palette_name = "polaroid", n = NULL, type = "discrete", ...) {
   pal <- suezalla_palette(name = palette_name, n = n, type = type)
@@ -17,6 +21,14 @@ scale_colour_suezalla <- function(palette_name = "polaroid", n = NULL, type = "d
 
 #' Custom ggplot2 fill scale for suezalla palettes
 #'
+#' Applies a named suezalla palette to the fill aesthetic in ggplot2.
+#'
+#' @param palette_name Name of the palette to use (e.g., "autumn", "vibrant")
+#' @param n Number of colours to use. If NULL, uses full palette length.
+#' @param type Either "discrete" or "continuous"
+#' @param ... Additional arguments passed to scale_fill_manual() or scale_fill_gradientn()
+#'
+#' @return A ggplot2 scale object
 #' @export
 scale_fill_suezalla <- function(palette_name = "polaroid", n = NULL, type = "discrete", ...) {
   pal <- suezalla_palette(name = palette_name, n = n, type = type)
