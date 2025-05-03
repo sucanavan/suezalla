b54f3bb3da3510a5c6cc39a7e89ff86dfb7703cf
sucanavan <48557623+sucanavan@users.noreply.github.com>
2025-05-03 15:38:40 +0100

    Change load to attach

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,10 @@
-.onLoad <- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
   if (requireNamespace("sysfonts", quietly = TRUE) &&
       requireNamespace("showtext", quietly = TRUE)) {
     sysfonts::font_add_google("Roboto", "Roboto")
     showtext::showtext_auto()
+    packageStartupMessage("Roboto font loaded for suezalla theme.")
+  } else {
+    packageStartupMessage("Install 'sysfonts' and 'showtext' to enable custom fonts.")
   }
 }
